aca99be067f16ed8f1d831753757b1a7643efc10
Ilya <61078603+ilya-07@users.noreply.github.com>
2021-01-26 21:21:41 +0300

    Delete know directory

--- know/script.js
+++
@@ -1,9 +0,0 @@
-$('.header__burger').click(function(event){
-    $('.header__burger, .h-menu__list, .header, .header__btn, .header__text, .header__title, .phone-6').toggleClass('burger-active');
-    $('body').toggleClass('lock');
-});
-$(document).ready(function(){
-    $('.cust__slides').slick({
-        slidesToShow:3
-    });
-});
