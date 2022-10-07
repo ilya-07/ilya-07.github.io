8a7996ba79523cec00c46abf6f30c00acfa7dbad
ilya <katacyri@mail.ru>
2022-10-07 23:12:29 +0300

    console.log text

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 const block = document.querySelector('.blick')
 block.innerHTML = '12345';
+const firstObj = {
+   num: 23,
+   name: 'ivan'
+};
+
+const secondObj = { ...firstObj }
+secondObj.name = 55
+
+console.log(firstObj.name, secondObj.name);
