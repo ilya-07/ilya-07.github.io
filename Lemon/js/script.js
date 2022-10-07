a89f322479346a6b4ab2e7979ba123de219189cf
Ilya <61078603+ilya-07@users.noreply.github.com>
2022-10-08 01:00:33 +0300

    Delete Lemon directory

--- Lemon/js/script.js
+++
@@ -1,8 +0,0 @@
-
-$(Document).ready(function(e){
-    $('.header__burger').click(function(event){
-        $('.header__burger, .h-menu__list ').toggleClass('burger-active');
-        $('body').toggleClass('lock');
-    });
-});
-
