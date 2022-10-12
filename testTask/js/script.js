6e33722e565269bee6cc00ffe39de7d2fb163479
ilya <katacyri@mail.ru>
2022-10-13 02:38:29 +0300

    header finished

---
+++ testTask/js/script.js
@@ -0,0 +1,8 @@
+'use strict'
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+
+burger.addEventListener('click', () => {
+   menu.classList.toggle('active');
+   burger.classList.toggle('active');
+})
