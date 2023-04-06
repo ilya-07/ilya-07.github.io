b14662b61f04d9a76ea46f769875f5c02d8d545c
Ilya <61078603+ilya-07@users.noreply.github.com>
2023-04-06 23:46:59 +0300

    Delete testTask directory

--- testTask/comeback/script.js
+++
@@ -1,5 +0,0 @@
-
-$(document).ready(function () {
-   $(document).mouseleave(function (e) { if (e.clientY < 0) { $(".outss").addClass("outssye"); } });
-   $('.outsscl, .outssback').click(function () { if ($(".outss").hasClass('outssye')) { $(".outss").css('display', 'none'); } });
-});
