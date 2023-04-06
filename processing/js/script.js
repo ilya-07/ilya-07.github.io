fba9cd7b3aa9153cd62551bddad24b5e9a5f5a9f
Ilya <61078603+ilya-07@users.noreply.github.com>
2023-04-06 23:46:08 +0300

    Delete processing directory

--- processing/js/script.js
+++
@@ -1,9 +0,0 @@
-// 'use strict'
-
-const burger = document.querySelector('.burger');
-const flow = document.querySelector('.header__flow');
-
-burger.addEventListener('click', () => {
-   burger.classList.toggle('active');
-   flow.classList.toggle('active');
-})
